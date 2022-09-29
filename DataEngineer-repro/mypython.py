a9c7fc88399ac1fb9b9268fc003b17b1edb4cd20
dmgautier85 <dmgautier@yahoo.com>
2022-09-29 14:02:28 -0600

    new change

--- DataEngineer-repro/mypython.py
+++ DataEngineer-repro/mypython.py
@@ -20,7 +20,8 @@ textbox.pack()
 button = tk.Button(root, text="Click Me Button", font=('Arial', 10))
 button.pack(padx=10,pady=10)
 
-#my comments 1
+
+# simple comment
 #myentry = tk.Entry(root)
 #myentry
 
